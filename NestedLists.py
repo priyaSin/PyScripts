e73554345608527916cc095a0f99344a00b78065
Priya Singh <priyaSin@users.noreply.github.com>
2017-06-08 17:18:54 +0530

    Added comments

--- NestedLists.py
+++ NestedLists.py
@@ -1,13 +1,29 @@
-""" Given the names and grades for each student in a Physics class of N students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+"""  Given the names and grades for each student in a Physics class of N
+students, store them in a nested list and print the name(s) of any student(s)
+having the second lowest grade.
 
 Note: If there are multiple students with the same grade, order their names
-alphabetically and print each name on a new line."""
+alphabetically and print each name on a new line.
+
+****Input Format****
+
+The first line contains an integer,N , the number of students.  The 2N subsequent
+lines describe each student over 2 lines; the first line contains a student's
+name, and the second line contains their grade.
+
+****Output Format****
+
+Print the name(s) of any student(s) having the second lowest grade in Physics;
+if there are multiple students, order their names alphabetically and print
+each one on a new line.
+
+"""
 
 arr=[]
 lowest=100
 seclowest=100
 for i in range(int(input("Enter the number of students in Physics class: "))):
-    print ("Enter the name of student number"+str(i))
+    print ("Enter the name of student number "+str(i))
     name = input()
     print ("Enter the score of "+name)
     score = float(input())
